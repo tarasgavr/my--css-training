aecf95393482337e12929a1cd76d987f0baaeedd
taureau <tarasgavrilov1985@gmail.com>
2023-07-20 18:37:58 +0300

    css-train-transform added

--- src/route/index.js
+++ src/route/index.js
@@ -17,6 +17,18 @@ router.get('/', function (req, res) {
 })
 
 // ================================================================
+router.get('/css-train-transform', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('css-train-transform', {
+    title: 'css-train-transform',
+    layout: 'css',
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
 
 // Підключаємо роутер до бек-енду
 module.exports = router
